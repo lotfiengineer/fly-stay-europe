dbbe314301b7ec63ac9cbcef9226228d5dda2ee2
mohammadlotfi <mohammad.lotfi7913@gmail.com>
2025-04-25 22:04:45 +0330

    implement finalcta and footer

---
+++ app/components/FinalCta/index.tsx
@@ -0,0 +1,33 @@
+import venice from "@/app/assets/countries/venice.jpg";
+import Image from "next/image";
+
+const FinalCta = () => {
+  return (
+    <div className="relative mx-auto flex h-[520px] w-full max-w-[1920px] justify-center px-10 max-[695px]:h-[670px]">
+      <div className="h-full rounded-2xl bg-black">
+        <Image
+          src={venice}
+          alt="venice"
+          className="h-full rounded-2xl object-cover opacity-30"
+        />
+      </div>
+      <div className="absolute flex h-full w-full flex-col items-center justify-center gap-9 rounded-2xl px-10 py-8 text-white">
+        <div className="text-h1 px-4 text-center font-bold">
+          Your Next European Adventure Awaits!
+        </div>
+        <div className="w-full max-w-[635px] px-4 text-center text-4xl">
+          Picture yourself strolling through the streets of Paris, exploring the
+          canals of Venice, or relaxing on a beach in Greece. Make it happen
+          today!
+        </div>
+        <div className="px-4">
+          <button className="bg-primary-normal text-h3 rounded-2xl px-16 py-3 font-bold text-white">
+            Start Your Journey
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FinalCta;
---
+++ app/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="mx-auto w-full max-w-[1920px] border-t">
+      <div className="text-h2 py-12 text-center font-bold">
+        Designed and developed by Mohammad Lotfi (lotfiengineer)
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
