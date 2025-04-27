740b7bdd24480ff9fb7e39e66415c062db575ee9
Mohammad Lotfi <mohammad.lotfi7913@gmail.com>
2025-04-27 18:44:47 +0330

    implemented how it works part

--- app/components/HowItWorks/ProcessStep.tsx
+++ app/components/HowItWorks/ProcessStep.tsx
@@ -2,32 +2,95 @@ import Image from "next/image";
 import React from "react";
 
 import destination from "@/app/assets/steps/destination.png";
-// import flight from "@/app/assets/steps/flight.png";
-// import hotel from "@/app/assets/steps/hotel.png";
+import flight from "@/app/assets/steps/flight.png";
+import hotel from "@/app/assets/steps/hotel.png";
+
+const data = [
+  {
+    title: "Choose a destination",
+    description:
+      "Pick from the best European countries and start planning your adventure.",
+    stepNumber: "01",
+    icon: destination,
+  },
+  {
+    title: "Book your flight",
+    description:
+      "Find the best deals on flights and secure your trip hassle-free",
+    stepNumber: "02",
+    icon: flight,
+  },
+  {
+    title: "Find the perfect hotel",
+    description: "Stay in top-rated hotels that fit your style and budget.",
+    stepNumber: "03",
+    icon: hotel,
+  },
+];
 
 const ProcessStep = () => {
   return (
     <div className="mx-auto w-full max-w-[980px]">
-      <div className="flex w-full flex-col">
-        <div className="grid grid-cols-[1fr_200px_1fr] items-center justify-items-center gap-5">
-          <div className="flex w-full max-w-72 flex-col">
-            <div className="text-h3 mb-5 font-bold">Choose a destination</div>
-            <div className="text-paragraph-large mb-10">
-              Pick from the best European countries and start planning your
-              adventure.
-            </div>
-            <div className="font-bold">- - - - - - - - - - - - - - - - - - - - - - - - - -</div>
+      <div className="relative flex w-full flex-col gap-14">
+        {/* Vertical line */}
+        <div className="bg-neutral-light absolute top-0 left-1/2 -z-10 h-full w-px -translate-x-1/2" />
+
+        {data.map((item, index) => (
+          <div
+            key={index}
+            className="relative z-10 grid grid-cols-[1fr_200px_1fr] items-center justify-items-center gap-5 "
+          >
+            {index === 1 ? (
+              <>
+                {/* Image First for second item */}
+                <div className="w-full max-w-72">
+                  <Image src={item.icon} alt={item.title} />
+                </div>
+                <div className="shadow-container border-primary-normal shadow-primary-light/60 flex h-min w-min flex-col justify-around rounded-3xl p-2 px-6 bg-white">
+                  <div className="text-h2 text-center font-bold text-[#8E8E8E]">
+                    STEP
+                  </div>
+                  <div className="text-h1 text-center font-bold">
+                    {item.stepNumber}
+                  </div>
+                </div>
+                <div className="flex w-full max-w-72 flex-col">
+                  <div className="text-h3 mb-5 font-bold">{item.title}</div>
+                  <div className="text-paragraph-large mb-10">
+                    {item.description}
+                  </div>
+                  <div className="font-bold">
+                    - - - - - - - - - - - - - - - - - - - - - - - - - -
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                {/* Normal order for others */}
+                <div className="flex w-full max-w-72 flex-col">
+                  <div className="text-h3 mb-5 font-bold">{item.title}</div>
+                  <div className="text-paragraph-large mb-10">
+                    {item.description}
+                  </div>
+                  <div className="font-bold">
+                    - - - - - - - - - - - - - - - - - - - - - - - - - -
+                  </div>
+                </div>
+                <div className="shadow-container border-primary-normal shadow-primary-light/60 flex h-min w-min flex-col justify-around rounded-3xl p-2 px-6 bg-white">
+                  <div className="text-h2 text-center font-bold text-[#8E8E8E]">
+                    STEP
+                  </div>
+                  <div className="text-h1 text-center font-bold">
+                    {item.stepNumber}
+                  </div>
+                </div>
+                <div className="w-full max-w-72">
+                  <Image src={item.icon} alt={item.title} />
+                </div>
+              </>
+            )}
           </div>
-          <div className="shadow-container border-primary-normal shadow-primary-light/60 flex h-min w-min flex-col justify-around rounded-3xl p-2 px-6">
-            <div className="text-h2 text-center font-bold text-[#8E8E8E]">
-              STEP
-            </div>
-            <div className="text-h1 text-center font-bold">01</div>
-          </div>
-          <div className="w-full max-w-72">
-            <Image src={destination} alt="destination" className="" />
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
+import CountriesSuggestion from "./components/CountriesSuggestions";
+import FinalCta from "./components/FinalCta";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 import HowItWorks from "./components/HowItWorks";
+import Reviews from "./components/Reviews";
+import WhyChooseUs from "./components/WhyChooseUs";
 
 export default function Home() {
   return (
     <div className="flex w-full flex-col justify-center">
-      {/* <Hero />
-      <CountriesSuggestion /> */}
+      <Hero />
+      <CountriesSuggestion />
 
       <HowItWorks />
 
-      {/* <WhyChooseUs />
+      <WhyChooseUs />
       <Reviews />
       <FinalCta />
-      <Footer /> */}
+      <Footer />
     </div>
   );
 }
